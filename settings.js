12e42d7c9235d06fcdc2efb0b22f28e642272499
prorizu786 <prorizvaan@gmail.com>
2024-07-28 15:54:42 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = 'jahaz Bot MD V11' //ur bot name
+global.ownernumber = '923286885027' //ur owner number
+global.ownername = '🦄' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
